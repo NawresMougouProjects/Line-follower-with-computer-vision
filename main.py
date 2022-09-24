d3e38e091243cf4354791ad0784f12c09dfbbdf5
nawres mougou <62567205+NawresMougouProjects@users.noreply.github.com>
2022-09-24 18:02:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+video = cv2.VideoCapture("vid.mp4")
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        video = cv2.VideoCapture("vid.mp4")
+        continue
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    low_black = np.array([0, 0, 0])
+    up_black = np.array([180, 50, 100])
+    mask = cv2.inRange(hsv, low_black, up_black)
+    edges = cv2.Canny(mask, 75, 150)
+
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=50)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cv2.imshow("frames", frame)
+    cv2.imshow("edges", edges)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+video.release()
+cv2.destroyAllWindows()
